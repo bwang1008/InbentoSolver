77ff912e9acb928d7e77da7cbaa00a81f0f48619
bwang1008 <bwang1008@gmail.com>
2024-07-13 00:49:21 -0500

    Remove redundant test command

--- main.py
+++ main.py
@@ -3,15 +3,16 @@
 from __future__ import annotations
 
 from pathlib import Path  # noqa: TCH003
+from typing import TYPE_CHECKING
 
 import typer
 from rich.progress import Progress, SpinnerColumn, TextColumn
 
-from inbento_solver.board import Board
 from inbento_solver.level import parse_level
-from inbento_solver.move import LiteralMove, Move
 from inbento_solver.solver import Solver
-from inbento_solver.tiles import Tile
+
+if TYPE_CHECKING:
+    from inbento_solver.move import Move
 
 app = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]})
 
@@ -31,34 +32,6 @@ def solve(level_file: Path) -> None:
     ) as progress:
         progress.add_task(description="[green]Searching...", total=None)
         history: list[tuple[Move, Move, int, int]] = solver.solve()
-
-    print("History:")
-    for index, move_information in enumerate(history):
-        if move_information[0] == move_information[1]:
-            print(
-                f"{index + 1}: Apply {move_information[1]} at "
-                f"({move_information[2]}, {move_information[3]})"
-            )
-        else:
-            print(
-                f"{index + 1}: Rotate {move_information[0]} to "
-                "{move_information[1]}; apply at ({move_information[2]}, "
-                "{move_information[3]})"
-            )
-
-
-@app.command()
-def test() -> None:
-    """Solves a dummy puzzle."""
-    initial_board: Board = Board([[Tile.RICE, Tile.RICE], [Tile.RICE, Tile.RICE]])
-    finish_board: Board = Board([[Tile.RICE, Tile.SALMON], [Tile.EGG, Tile.SALMON]])
-    moves: list[Move] = [
-        LiteralMove({(0, 0): Tile.SALMON, (0, 1): Tile.SALMON}, False),
-        LiteralMove({(0, 0): Tile.EGG, (0, 1): Tile.EGG}, False),
-    ]
-
-    solver: Solver = Solver(initial_board, finish_board, moves)
-    history: list[tuple[Move, Move, int, int]] = solver.solve()
 
     print("History:")
     for index, move_information in enumerate(history):
