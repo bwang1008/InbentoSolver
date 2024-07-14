78b231682e710ecdf4aed992ccf14ef6aa03b3cf
bwang1008 <bwang1008@gmail.com>
2024-07-14 12:47:17 -0500

    Board constructor now takes in 2D list of strings

--- inbento_solver/board.py
+++ inbento_solver/board.py
@@ -2,25 +2,31 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, cast
+from typing import List, cast
 
 from typing_extensions import Self
 
-if TYPE_CHECKING:
-    from .tiles import Tile
+from inbento_solver.tiles import Tile
 
 
 class Board:
     """Board class that represents a grid of tiles."""
 
-    def __init__(self: Self, tiles: list[list[Tile]]) -> None:
+    def __init__(self: Self, tiles: list[list[Tile]] | list[list[str]]) -> None:
         """Create instance of Board class.
 
         Input is a 2D array of tiles.
         """
         self.R = len(tiles)
         self.C = len(tiles[0])
-        self.tiles = tiles
+        self.tiles: list[list[Tile]] = []
+
+        if isinstance(tiles[0][0], Tile):
+            tiles = cast(List[List[Tile]], tiles)
+            self.tiles = tiles
+        else:
+            tiles = cast(List[List[str]], tiles)
+            self.tiles = [[Tile[tile_info] for tile_info in row] for row in tiles]
 
     def __eq__(self: Self, other: object) -> bool:
         """Check equality between Board instances."""
