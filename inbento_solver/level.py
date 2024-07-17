d0f987e98f8cb57c8693f8b17512e37266d80792
bwang1008 <bwang1008@gmail.com>
2024-07-16 23:58:13 -0500

    Use pydantic to parse level from JSON

--- inbento_solver/level.py
+++ inbento_solver/level.py
@@ -3,37 +3,30 @@
 from __future__ import annotations
 
 import json
-from typing import TYPE_CHECKING, Dict, List, Union, cast
+from typing import TYPE_CHECKING
 
-from inbento_solver.board import Board
-from inbento_solver.moves.literal import LiteralMove
+from pydantic import BaseModel
 
 if TYPE_CHECKING:
     from pathlib import Path
 
+    from inbento_solver.board import Board
     from inbento_solver.moves.base import Move
 
 
-def parse_level(level_path: Path) -> tuple[str, Board, Board, list[Move]]:
+class Level(BaseModel):
+    """Class description of info in a puzzle level."""
+
+    title: str
+    start: Board
+    finish: Board
+    moves: list[Move]
+
+
+def parse_level(level_path: Path) -> Level:
     """Extract level data from JSON file."""
     with level_path.open(mode="r", encoding="utf-8") as level_file:
         level_info = json.load(level_file)
 
-    title: str = level_info["title"]
-    start_board: Board = Board(level_info["start"])
-    finish_board: Board = Board(level_info["finish"])
-    moves_info: list[dict[str, str | dict[str, bool | list[list[str]]]]] = level_info[
-        "moves"
-    ]
-
-    moves: list[Move] = []
-
-    for move_info in moves_info:
-        if move_info["type"] == "literal":
-            move_data: dict[str, bool | list[list[str]]] = cast(
-                Dict[str, Union[bool, List[List[str]]]], move_info["data"]
-            )
-
-            moves.append(LiteralMove.from_json(move_data))
-
-    return title, start_board, finish_board, moves
+    level: Level = Level.model_validate(level_info)
+    return level
--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from inbento_solver.level import parse_level
 from inbento_solver.solver import Solver
 
 if TYPE_CHECKING:
+    from inbento_solver.level import Level
     from inbento_solver.moves.base import MoveDescription
 
 app = typer.Typer(context_settings={"help_option_names": ["-h", "--help"]})
@@ -26,8 +27,8 @@ def solve(level_file: Path) -> None:
         this will be fixed when https://github.com/tiangolo/typer/pull/814
         is in the next release.
     """
-    title, start_board, finish_board, moves = parse_level(level_file)
-    solver: Solver = Solver(start_board, finish_board, moves)
+    level: Level = parse_level(level_file)
+    solver: Solver = Solver(level.start, level.finish, level.moves)
 
     with Progress(
         SpinnerColumn(),
@@ -37,7 +38,7 @@ def solve(level_file: Path) -> None:
         progress.add_task(description="[green]Searching...", total=None)
         history: list[MoveDescription] = solver.solve()
 
-    print(f"Steps to solve level {title}:")
+    print(f"Steps to solve level {level.title}:")
     for index, move_information in enumerate(history):
         print(f"{index + 1}: {move_information}")
 
