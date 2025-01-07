a6aba3737e530635fd9979a0c39c565839cb5991
bwang1008 <bwang1008@gmail.com>
2025-01-06 23:39:47 -0600

    Finish implementing swap

--- inbento_solver/moves/swap.py
+++ inbento_solver/moves/swap.py
@@ -7,10 +7,11 @@ from typing import TYPE_CHECKING, List, Literal
 from typing_extensions import Self
 
 from inbento_solver.moves.base import MoveBase
-from inbento_solver.tiles import TilePosition  # noqa: TC001
+from inbento_solver.tiles import TilePosition
 
 if TYPE_CHECKING:
     from inbento_solver.board import Board
+    from inbento_solver.tiles import Tile
 
 
 class SwapMove(MoveBase):
@@ -31,8 +32,41 @@ class SwapMove(MoveBase):
         self: Self, board: Board, start_pos: tuple[int, int]
     ) -> tuple[Board, MoveBase | None, bool]:
         """Directly apply the move's tile on top of the existing board tiles."""
-        raise NotImplementedError
+        board_copy = board.model_copy(deep=True)
+
+        pos1: tuple[int, int] = self.positions[0].pos
+        row1: int = pos1[0] + start_pos[0]
+        col1: int = pos1[1] + start_pos[1]
+        if not board.is_valid_position(row1, col1):
+            return board, None, False
+
+        pos2: tuple[int, int] = self.positions[1].pos
+        row2: int = pos2[0] + start_pos[0]
+        col2: int = pos2[1] + start_pos[1]
+        if not board.is_valid_position(row2, col2):
+            return board, None, False
+
+        orig: Tile = board_copy.tiles[row1][col1]
+        board_copy.tiles[row1][col1] = board_copy.tiles[row2][col2]
+        board_copy.tiles[row2][col2] = orig
+
+        return board_copy, None, True
 
     def rotate_counter_clockwise(self: Self) -> SwapMove:
         """Turn the set of tiles in the move."""
-        raise NotImplementedError
+        if self.locked:
+            return self
+
+        max_col: int = max(tile_position.pos[1] for tile_position in self.positions)
+
+        new_positions: list[TilePosition] = []
+        for tile_position in self.positions:
+            pos: tuple[int, int] = tile_position.pos
+            tile: Tile = tile_position.tile
+
+            new_tile_position: TilePosition = TilePosition(
+                pos=(max_col - pos[1], pos[0]), tile=tile
+            )
+            new_positions.append(new_tile_position)
+
+        return SwapMove(positions=new_positions, locked=self.locked)
